f03a4ccf5fabe4448442617cd1fd81a8ae1faf84
Tyson910 <45476882+Tyson910@users.noreply.github.com>
2022-06-01 18:37:20 -0500

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    preact(),
+    react(),
+    tailwind({
+      // Example: Disable injecting a basic `base.css` import on every page.
+      // Useful if you need to define and/or import your own custom `base.css`.
+      // config: { applyBaseStyles: false },
+    }),
+  ],
+});
