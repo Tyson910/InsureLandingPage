6922684b3eba8c6a26aec430c78c24a6771f9ded
Tyson910 <Tyson910@lve.missouristate.edu>
2022-07-10 11:16:39 -0400

    add google font to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        sansSerif: ['Karla'],
+        serif : ['DM Serif Display'],
+      }
+    },
   },
   plugins: [],
 };
