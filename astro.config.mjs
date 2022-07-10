2bca5d9638adea20d42578fe28db7330d38af925
Tyson910 <Tyson910@lve.missouristate.edu>
2022-07-10 11:19:27 -0400

    Disable injecting a basic `base.css` import on every page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
@@ -12,7 +11,9 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      // config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
   ],
 });
