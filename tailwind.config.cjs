feefec56afd7dcecec4c92d2c4bf88b1569e5105
Tyson910 <Tyson910@lve.missouristate.edu>
2022-07-10 11:18:46 -0400

    add dark-violet to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        'dark-violet':'hsl(256, 26%, 20%)'
+      },
       fontFamily : {
         sansSerif: ['Karla'],
         serif : ['DM Serif Display'],
